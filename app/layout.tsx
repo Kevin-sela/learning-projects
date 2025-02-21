e064fd4f9a1152c21ff1b8299081ffd51f62f17b
kevin sela <princeowusu.272@gmail.com>
2025-02-21 10:08:34 +0000

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import "styles/globals.css";
-import { Metadata, Viewport } from "next";
+import { Metadata } from "next";
 import { Link } from "@heroui/link";
 import clsx from "clsx";
 
-import { Providers } from "./providers";
 import { siteConfig } from "config/site";
 import { fontSans } from "config/fonts";
 import Navbar from "app/components/navbar";
@@ -17,13 +16,6 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
-};
-
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
 };
 
 export default function RootLayout({
@@ -40,13 +32,12 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
+        <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="container flex-grow px-6 pt-16 mx-auto max-w-7xl">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
+            <footer className="flex items-center justify-center w-full py-3">
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
@@ -58,7 +49,6 @@ export default function RootLayout({
               </Link>
             </footer>
           </div>
-        </Providers>
       </body>
     </html>
   );
