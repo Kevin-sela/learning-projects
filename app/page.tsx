62de3f823e0d7256767dca20d80bd4bf92ebd1b7
kevin sela <princeowusu.272@gmail.com>
2025-02-21 01:41:35 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import Table from "../app/components/Table";
 
 export default function Page() {
   return (
-    <div className="container px-4 py-8 mx-auto min-h-screen">
+    <div className="container px-4 py-8 mx-auto min-h-screen bg-[url('/pictures/51380.jpg')] bg-cover bg-center bg-no-repeat">
+
       <div className="mb-8">
         <h1 className="text-4xl font-extrabold text-center text-transparent bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text animate-gradient-x">
           CRUD
